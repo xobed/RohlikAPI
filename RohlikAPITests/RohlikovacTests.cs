63773ccbc5b5663cd9c5d18374438f8212ca3554
notdev <notdev@users.noreply.github.com>
2016-03-03 18:33:48 +0100

    Add Rohlikovac test

---
+++ RohlikAPITests/RohlikovacTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RohlikAPISharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RohlikAPISharp.Tests
+{
+    [TestClass()]
+    public class RohlikovacTests
+    {
+        private string[] GetCredentials()
+        {
+            var passwordString = File.ReadAllText("loginPassword.txt");
+            return passwordString.Split(':');
+        }
+
+        [TestMethod()]
+        public void RunTest()
+        {
+            var login = GetCredentials();
+            var rohlikApi = new RohlikApi(login[0], login[1]);
+            string result = rohlikApi.RunRohlikovac();
+            if (result.Contains("error"))
+            {
+                Assert.Fail($"Failed to login to rohlik. Login used: {login[0]}");
+            }
+        }
+    }
+}
