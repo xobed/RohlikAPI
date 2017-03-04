73cf24612a9e89bb281a8b440e8920c19dd8de0b
Dusan Obert <obert.dusan@gmail.com>
2017-03-04 23:24:39 +0100

    Fix GetAllCategories

--- RohlikAPI/Categories.cs
+++ RohlikAPI/Categories.cs
@@ -19,8 +19,9 @@ namespace RohlikAPI
             var rohlikFrontDocument = new HtmlDocument();
             rohlikFrontDocument.LoadHtml(rohlikFrontString);
 
-            var categories = rohlikFrontDocument.DocumentNode.SelectNodes("//div[contains(@id,'rootcat')]/div/a");
-            return categories.Select(c => c.Attributes["href"].Value.Substring(1));
+            var categoryNodes = rohlikFrontDocument.DocumentNode.SelectNodes("//div[contains(@id,'rootcat')]/div/a");
+            var categoryHrefs = categoryNodes.Select(c => c.Attributes["href"].Value);
+            return categoryHrefs.Select(c => c.Substring(1));
         }
     }
 }
