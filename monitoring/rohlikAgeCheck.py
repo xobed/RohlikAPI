ab8c896825118d75ed92eead460856c52b4e5cf5
Dusan Obert <obert.dusan@gmail.com>
2018-03-22 20:42:35 +0100

    Simplify age check

--- monitoring/rohlikAgeCheck.py
+++ monitoring/rohlikAgeCheck.py
@@ -1,23 +1,12 @@
 from checks import AgentCheck
 import urllib2
-import dateutil.parser
-from datetime import datetime
-import pytz
-import json
-
 
 class RohlikAgeCheck(AgentCheck):
     def check(self, instance):
-        response = urllib2.urlopen('https://rohlikapi.azurewebsites.net/api/GetAllProducts')
+        response = urllib2.urlopen('https://rohlikapi.azurewebsites.net/api/GetAllProductsAge')
         response_content = response.read()
-
-        deserialized = json.loads(response_content)
-        time_string = deserialized['SyncTime']
-        time = dateutil.parser.parse(time_string)
-        timediff = datetime.now(pytz.utc) - time
-        hours = int(timediff.total_seconds() / 3600)
-        self.gauge('rohlikapi.productsage', hours)        
-
+        hours = int(response_content)
+        self.gauge('rohlikapi.productsage', hours)
 
 if __name__ == '__main__':
     check, instances = RohlikAgeCheck.from_yaml('/etc/dd-agent/conf.d/rohlikAgeCheck.yaml')
