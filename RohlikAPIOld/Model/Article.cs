66c9e04cdf6cd1303216f11feb1db3baa2c90ceb
Dusan Obert <obert.dusan@gmail.com>
2020-02-22 15:47:47 +0100

    Remove old files

--- RohlikAPIOld/Model/Article.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RohlikAPI.Model
-{
-    public class Article
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int Count { get; set; }
-    }
-}
